76961f40a764b7885831ffa1f5c76e6fd4964718
Tiger Oakes <contact@tigeroakes.com>
2021-02-12 15:42:27 -0800

    Add form and analytics

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,70 +1,72 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ['./layouts/**/*.html', './content/**/*.md'],
-  darkMode: 'media',
+  purge: {
+    enabled:
+      process.env.HUGO_ENVIRONMENT === "production" ||
+      process.env.NODE_ENV === "production",
+    content: ["./layouts/**/*.html", "./content/**/*.md"],
+  },
+  darkMode: "media",
   theme: {
     fontFamily: {
-      display: ['Red Rose', 'serif'],
+      display: ["Red Rose", "serif"],
     },
     extend: {
       backgroundSize: {
-        '96': '24rem',
+        96: "24rem",
       },
       inset: {
-        '1/4-screen': '25vh',
-        '3/4-screen': '75vh',
+        "1/4-screen": "25vh",
+        "3/4-screen": "75vh",
       },
       minHeight: {
-        '1/4-screen': '25vh',
+        "1/4-screen": "25vh",
       },
       colors: {
-        route: 'var(--route-color)',
+        route: "var(--route-color)",
         gray: {
           ...colors.trueGray,
-          750: '#333',
+          750: "#333",
         },
         blue: {
-          50: 'hsl(212, 20%, 90%)',
-          100: 'hsl(212, 10%, 72%)',
-          200: 'hsl(212, 10%, 64%)',
-          300: 'hsl(212, 10%, 53%)',
-          400: 'hsl(212, 10%, 42%)',
-          500: 'hsl(212, 10%, 32%)',
-          600: 'hsl(212, 10%, 28%)',
-          700: 'hsl(212, 10%, 22%)',
-          800: 'hsl(212, 10%, 18%)',
-          900: 'hsl(212, 10%, 10%)',
+          50: "hsl(212, 20%, 90%)",
+          100: "hsl(212, 10%, 72%)",
+          200: "hsl(212, 10%, 64%)",
+          300: "hsl(212, 10%, 53%)",
+          400: "hsl(212, 10%, 42%)",
+          500: "hsl(212, 10%, 32%)",
+          600: "hsl(212, 10%, 28%)",
+          700: "hsl(212, 10%, 22%)",
+          800: "hsl(212, 10%, 18%)",
+          900: "hsl(212, 10%, 10%)",
         },
         red: {
-          DEFAULT: '#C67168',
+          DEFAULT: "#C67168",
         },
         yellow: {
-          DEFAULT: '#E2C049',
+          DEFAULT: "#E2C049",
         },
         brown: {
-          DEFAULT: '#918381',
+          DEFAULT: "#918381",
         },
         cyan: {
-          DEFAULT: '#8BB9BD',
+          DEFAULT: "#8BB9BD",
         },
         ocean: {
-          light: '#778a91',
-          DEFAULT: '#335468',
-          dark: '#2d4859',
+          light: "#778a91",
+          DEFAULT: "#335468",
+          dark: "#2d4859",
         },
       },
       fill: (theme) => ({
-        'blue-500': theme('colors.blue.500'),
-        'blue-600': theme('colors.blue.600'),
+        "blue-500": theme("colors.blue.500"),
+        "blue-600": theme("colors.blue.600"),
       }),
       textColor: {
-        route: 'var(--route-text-color)',
+        route: "var(--route-text-color)",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require("@tailwindcss/forms")],
 };
